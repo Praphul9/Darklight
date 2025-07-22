268d92f1a1bc9e8a85176e292b42762be61ede7f
Praphul Kumar Pandey <pandey.praphulkumar@gmail.com>
2025-07-22 17:00:17 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return (
-    <footer className="bg-black text-center text-sm text-gray-500 py-4">
-      © 2025 Darklight Technology LTD | connect@darklighttech.com
-    </footer>
-  );
-}
